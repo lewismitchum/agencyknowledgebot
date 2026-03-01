5d480efdf07fc83a011eb86ec92cee659e445991
Lewis Mitchum <dabb75776@gmail.com>
2026-03-01 14:32:58 -0600

    Fix global Turnstile type collision

---
+++ types/turnstile.d.ts
@@ -0,0 +1,20 @@
+export {};
+
+declare global {
+  interface Window {
+    turnstile?: {
+      render: (
+        el: HTMLElement,
+        opts: {
+          sitekey: string;
+          theme?: "light" | "dark" | "auto";
+          callback?: (token: string) => void;
+          "error-callback"?: () => void;
+          "expired-callback"?: () => void;
+        }
+      ) => string;
+      reset: (widgetId: string) => void;
+      remove?: (widgetId: string) => void;
+    };
+  }
+}
